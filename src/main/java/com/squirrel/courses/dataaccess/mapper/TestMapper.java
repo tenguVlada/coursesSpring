be68c9a4b6cf6f87ee600a026cbd66801f16f668
PusheenTheCat <mewflonne@mail.ru>
2019-01-25 18:14:47 +0200

    Add test mapper

---
+++ src/main/java/com/squirrel/courses/dataaccess/mapper/TestMapper.java
@@ -0,0 +1,22 @@
+package com.squirrel.courses.dataaccess.mapper;
+
+import com.squirrel.courses.dataaccess.model.Test;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestMapper implements RowMapper<Test> {
+    public static final String BASE_SQL = "SELECT id, lesson, evaluation, isExam FROM test";
+    public static final String INSERT_SQL = "INSERT INTO test(lesson, evaluation, isExam)";
+
+    @Override
+    public Test mapRow(ResultSet resultSet, int i) throws SQLException {
+        int id = resultSet.getInt("id");
+        int lesson = resultSet.getInt("lesson");
+        int evaluation = resultSet.getInt("evaluation");
+        byte isExam = resultSet.getByte("isExam");
+
+        return new Test(id, lesson, evaluation, isExam);
+    }
+}
